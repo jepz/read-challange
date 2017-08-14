355973ab6eca44c0d7edea029af97543dc7e086f
jepz <jesper.lindholmer@avaloninnovation.com>
2017-08-14 16:27:35 +0200

    added App to stories, added placeholder for function in BookItem

--- app/components/BookItem.js
+++ app/components/BookItem.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class view extends React.Component {
+  onClick(eventArgs) {
+    //change the Book name and input filed to a text field.
+    console.log("checked!");
+  }
   render() {
     return (
       <div className="bookItem">
--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+import app from "../app/components/App";
 import bookItem from "../app/components/BookItem";
 
+storiesOf("Main App", module).add("default", () => <app.App />);
 storiesOf("Book Item", module).add("default", () => <bookItem.view />);
