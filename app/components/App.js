45f2890d3d0e15273af9f07eae9dc065f35c2916
jepz <jesper.lindholmer@avaloninnovation.com>
2017-08-14 13:55:37 +0200

    add folder app for development

---
+++ app/components/App.js
@@ -0,0 +1,7 @@
+var React = require("react");
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReacDOM = require("react-dom");
+
+require("./index.css");
+
+var App = require("./components/App");
+
+ReactDOM.render(<App />, document.getElementById("app"));
