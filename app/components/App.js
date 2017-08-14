47b515f46f636e5116fd91638703b4df9a6173dc
jepz <jesper.lindholmer@avaloninnovation.com>
2017-08-14 16:45:18 +0200

    added bookitem component and a todo

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import BookItem from "./BookItem";
+
+//TODO: add css-in-js, kolla på https://www.styled-components.com/docs/basics
 
 class App extends React.Component {
   render() {
-    return <h1>Hello world</h1>;
+    console.log(BookItem);
+    return (
+      <div className="app">
+        <h1>Reading challenge</h1>
+        <BookItem.view />
+      </div>
+    );
   }
 }
 
