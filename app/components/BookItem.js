5bdb83f8188176c211dc0a3e352ddca60a3f7b75
jepz <jesper.lindholmer@avaloninnovation.com>
2017-08-16 11:50:31 +0200

    change to a stateless function

--- app/components/BookItem.js
+++ app/components/BookItem.js
@@ -1,31 +1,22 @@
 import React from "react";
-
 import styled from "styled-components";
 
-//TODO: add css-in-js, kolla på https://www.styled-components.com/docs/basics
-
-class view extends React.Component {
-  onClick(eventArgs) {
-    //change the Book name and input filed to a text field.
-    console.log("checked!");
-  }
-  render() {
-    return (
-      <div className="bookItem">
-        <form>
-          <label>
-            <input type="checkbox" />
-            Challenge text
-          </label>
-          <br />
-          <label>
-            <input type="text" value="The books title" />
-          </label>
-          <button onClick={this.onClick}>Check</button>
-        </form>
-      </div>
-    );
-  }
+function BookItem(props) {
+  return (
+    <div className="bookItem">
+      <form>
+        <label>
+          <input type="checkbox" />
+          {props.title}
+        </label>
+        <br />
+        <label>
+          <input type="text" value="The books title" />
+        </label>
+        <button>Check</button>
+      </form>
+    </div>
+  );
 }
 
-export default { view };
+export default BookItem;
